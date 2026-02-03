abaad4ea0c3f4b58a13e6a1d06e47cf167b19ec3
Mattias Schertell <76884+mattische@users.noreply.github.com>
2026-02-03 07:14:49 +0100

    mode open

--- src/components/single-product.js
+++ src/components/single-product.js
@@ -4,7 +4,13 @@ export default class SingleProduct extends HTMLElement {
 
     constructor() {
         super();
-        //this.attachShadow({ mode: 'open' });
+
+        // mode: open - allows access to shadow DOM from outside the component
+        this.attachShadow({ mode: 'open' });
+        // mode: closed - prevents access to shadow DOM from outside the component.
+        // We create a refenrece (_shadow) to the shadow DOM in the constructor, so we later
+        // can refer to it using this._shadow.
+        //this._shadow = this.attachShadow({ mode: 'closed' });
 
     }
     // component attributes
@@ -26,12 +32,37 @@ export default class SingleProduct extends HTMLElement {
     }
     // connect component
     connectedCallback() {
-        //path to css realtive to html file (not this js file)
-        this.innerHTML = `<h4 class="product-name">${this.product.name}</h4> 
+
+        // shadowRoot:
+        // mode: 'open', we can access shadow DOM from outside the component using element.shadowRoot
+        // mode: 'closed', we cannot access shadow DOM from outside the component.
+        // If closed we would have to save an reference to the shadow DOM in the constructor.
+        // Example: this._shadow = this.attachShadow({ mode: 'closed' });
+        // Then we would use this._shadow instead of this.shadowRoot below.
+        // CSS:
+        // inline css is recommended for shadow DOM components.
+        // If we used an external stylesheet, it would loaded for each component that is fetched from the server.
+        this.shadowRoot.innerHTML = `
+                            <style>
+                            h4 { background: #e31c79; color: white; border-radius: 5px; padding: 10px; cursor: pointer; }
+                            img { width: 20%; }
+                            .product-description { font-size: 0.8rem; max-height: 0;
+                                overflow: hidden;
+                                transition: max-height 0.3s ease;
+                            }
+                            .product-description.show {
+                                max-height: 500px;
+                            }
+                            .read { color: black; text-decoration: underline; cursor: pointer; }
+                            </style>
+                            
+                            <h4 class="product-name">${this.product.name}</h4> 
                             <img src='${this.product.image_url}' alt='${this.product.name}' width='100' />
+                            <p class="read">Read description...</p>
                             <div class='product-description'>${renderMarkdown(this.product.description)}</div>`;
-        //this.shadowRoot.querySelector('.product-name').addEventListener('click', () => {
-        //  console.log(`Product clicked: ${this.product.name}`);
-        //});
+        this.shadowRoot.querySelector('.read').addEventListener('click', () => {
+            this.shadowRoot.querySelector('.product-description').classList.toggle('show')
+            console.log('clicked');
+        });
     }
 }
